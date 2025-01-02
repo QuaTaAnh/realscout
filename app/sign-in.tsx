a6e2b6af3e973a22d060d9eaf252bcfc5579b82c
anhltn <anh.ltn1@zinza.com.vn>
2025-01-02 16:47:30 +0700

    test

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,18 @@
-import { View, Text } from 'react-native'
+import images from '@/constants/images'
 import React from 'react'
+import { Image, ScrollView, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>sign-in</Text>
-    </View>
+    <SafeAreaView className='bg-white h-full'>
+      <ScrollView contentContainerClassName='h-full'>
+        <Image source={images.onboarding} className='w-full h-4/6' resizeMode='contain' />
+        <View className='px-10'>
+        <Text className='text-base text-center uppercase font-rubik text-black-200'>Welcome to Zinza</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
