ec5e3b6a528e0e08f4c51e26cc80c8db86ae6eb5
anhltn <anh.ltn1@zinza.com.vn>
2025-01-02 15:37:37 +0700

    setup nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import "./globals.css"
 
 export default function RootLayout() {
   const [loaded] = useFonts({
